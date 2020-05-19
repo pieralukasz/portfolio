ffc10aaeec25db599ae899c031e27ff5927aa4dd
pieras <pieralukasz@gmail.com>
2020-05-19 18:02:24 +0200

    initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackMd5Hash = require('webpack-md5-hash')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const { prod_Path, src_Path } = require('./config/path')
+
+module.exports = {
+    entry: {
+        main: './' + src_Path + '/index.ts',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, prod_Path),
+        filename: '[name].[chunkhash].js',
+    },
+    //devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(sass|scss|css)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(path.resolve(__dirname, prod_Path), {
+            root: process.cwd(),
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './' + src_Path + '/public/index.html',
+            filename: 'index.html',
+        }),
+        new WebpackMd5Hash(),
+    ],
+}
