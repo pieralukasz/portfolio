ea459acde59a5ede165b24b5a8070cb88641e567
pieras <pieralukasz@gmail.com>
2020-05-26 22:11:36 +0200

    depoyHeroku

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// here will be server for heroku
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const history = require('connect-history-api-fallback');
+
+const app = express()
+app.use(history());
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+const port = process.env.PORT || 8000
+
+app.listen(port)
+
+
+console.log('Listening on port: ' + port)
